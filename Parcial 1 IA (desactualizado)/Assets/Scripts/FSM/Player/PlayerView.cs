43a044517842ddf2a32181e1125659740be01bc9
XLordDeusX <wilfredoa.rukoz@gmail.com>
2023-04-22 20:28:33 -0300

    Graphics

--- Parcial 1 IA (desactualizado)/Assets/Scripts/FSM/Player/PlayerView.cs
+++ Parcial 1 IA (desactualizado)/Assets/Scripts/FSM/Player/PlayerView.cs
@@ -6,4 +6,14 @@ public class PlayerView : MonoBehaviour
 {
     Rigidbody _rb;
     public Animator anim;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        var vel = _rb.velocity.magnitude;
+        anim.SetFloat("Vel", vel);
+    }
 }
